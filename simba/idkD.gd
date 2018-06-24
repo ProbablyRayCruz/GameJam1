58b9109c4719b92dfac31d4b71ac5061531de60e
Simbalogy <simbahmed@gmail.com>
2018-06-24 00:17:58 -0700

    lvl1

--- simba/idkD.gd
+++ simba/idkD.gd
@@ -9,21 +9,21 @@ const MOTION_SPEED = 300 # Pixels/second
 var motion = Vector2()
 
 func _physics_process(delta):
-	
-	
+
+
 	if Input.is_action_pressed("ui_up"):
 		motion.x = MOTION_SPEED
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -MOTION_SPEED
-	
+
 	#if Input.is_action_pressed("ui_down"):
 	#	motion += Vector2(0, 1)
-	
+
 	#if Input.is_action_pressed("ui_left"):
 	#	motion += Vector2(-1, 0)
 	#if Input.is_action_pressed("ui_right"):
 	#	motion += Vector2(1, 0)
-	
+
 	#motion = motion.normalized() * MOTION_SPEED
 
 	move_and_slide(motion)
